09b9497b767434ebe9df6d7f84ab9295edf8df88
whd <754429515@qq.com>
2019-11-26 14:45:27 +0800

    新建 springboot + mqtt 项目

---
+++ mirror_processor/src/main/java/com/joydigit/mirror_processor/config/MqttReceiveConfig.java
@@ -0,0 +1,13 @@
+package com.joydigit.mirror_processor.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+
+/**
+ * create by 75442 on 2019/11/26
+ */
+@Configuration
+@IntegrationComponentScan
+@Slf4j
+public class MqttReceiveConfig {
+}
